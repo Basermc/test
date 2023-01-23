40e00ba331afdf27f229be3be003946a32eb4d2b
Basermc <50556356+Basermc@users.noreply.github.com>
2023-01-23 16:25:11 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,56 +1,45 @@
-#!/bin/bash
-
-RED='\033[0;31m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
-
-
-# Prompt for namespace
-read -p "Enter the namespace: " namespace
-
-# Prompt for group size
-read -p "Enter the group size (max 20): " group_size
-
-#Prompt project
-read -p "Enter the project name:" project
-
-
-# Validate group size
-if [ $group_size -gt 20 ]; then
-    echo -e "${RED}Error:${NC} Group size cannot be greater than 20."
-    exit 1
-fi
-
-# Obtiene una lista de todos los despliegues en el namespace
-deployments=($(oc get dc -n $namespace -o jsonpath='{.items[*].metadata.name}'))
-
-# Define el contador
-counter=0
-
-# Define el índice del despliegue actual
-index=0
-
-# Obtiene el número total de despliegues
-total_deployments=${#deployments[@]}
-
-
-# Itera mientras queden despliegues por reiniciar
-while [ $index -lt $total_deployments ]; do
-    # Reinicia el despliegue actual
-    if ! oc rollout latest -n $namespace dc/${deployments[$index]}; then
-        echo -e "${RED}Error:${NC} Deployment ${deployments[$index]} failed."
-    fi
-    counter=$((counter+1))
-    index=$((index+1))
-    # Si el contador alcanza el tamaño del grupo o no quedan más despliegues por reiniciar, espera antes de continuar
-    if [ $counter -eq $group_size ] || [ $index -eq $total_deployments ]; then
-        counter=0
-        # espera hasta que se complete el despliegue anterior
-        oc rollout status -w -n $namespace dc/${deployments[$index-1]}
-        #sleep 10s
-    fi
+while getopts s:t:p:i:l:g: flag; do
+  case "${flag}" in
+  s) server=${OPTARG} ;;
+  t) token=${OPTARG} ;;
+  p) project=${OPTARG} ;;
+  i) interval=${OPTARG} ;;
+  l) label=${OPTARG} ;;
+  esac
 done
 
-# Success message
-echo -e "${BLUE}Deployment completed successfully!${NC}"
 
+echo "Project: $project"
+echo "Group size: $group_size"
+
+oc login -u system:admin -n ${project}
+
+deployments="./oc.exe get deploymentconfig -o name  -l '$label'"
+values=$(eval "$deployments")
+
+# Define the counter and total number of deployments
+counter=0
+total_deployments=${#values[@]}
+
+for dc in ${values}
+do
+   echo "Restart $dc "
+   pod="$(cut -d'/' -f2 <<<$dc)"
+   # Validate that the total number of deployments does not exceed 20
+   if [ $counter -lt 20 ]; then
+        sleep $interval
+        # Check the counter value before restarting
+        if [ $(($counter % 2)) -eq 0 ]; then
+            oc rollout latest  $dc
+            oc rollout.exe status $dc --timeout=10m
+        else
+            oc rollout latest  $dc
+            oc rollout status $dc --timeout=10m
+        fi
+        counter=$((counter+1))
+   else
+        echo "Total number of deployments exceeded 20"
+        exit 1
+   fi
+done
+oc delete pods --field-selector status.phase!=Running
