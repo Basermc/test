9ed42e58c5d3ee7f596a912617021607611cc379
Basermc <50556356+Basermc@users.noreply.github.com>
2023-01-24 10:50:08 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -25,7 +25,7 @@ if [ ${group_size} -gt 20 ]; then
 fi
 
 # Obtiene una lista de todos los despliegues en el namespace
-deployments=($(oc get dc -n ${project} -o name ))
+deployments=($(oc get dc -n ${project} -o jsonpath='{.items[*].metadata.name}'
 
 # Define el contador
 counter=0
