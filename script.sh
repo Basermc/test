93b21a66fae7fe20dfbda80940d67dbc64298f88
Basermc <50556356+Basermc@users.noreply.github.com>
2023-01-24 11:05:55 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,8 +13,7 @@ done
 
 echo "Project: $project"
 echo "Group size: $group_size"
-echo "Server: $server"
-oc login -u system:admin -n $project --server=$server
+oc login -u system:admin -n $project
 
 # Validate group size
 if [ ${group_size} -gt 20 ]; then
